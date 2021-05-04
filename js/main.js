7a5ba63abe351e33a28fed21ad21af798de0afd2
Tabitha Bjorkman <tabithabjorkman@gmail.com>
2021-05-04 17:39:33 +0200

    session js

--- js/main.js
+++ js/main.js
@@ -4,6 +4,16 @@ window.addEventListener('load', () => {
     const preload = document.querySelector('.preload');
     preload.classList.add("preload-finish");
 });
+// window.addEventListener("load", () => {
+//           if (!sessionStorage.viewed){
+//               const preload = document.querySelector(".preload");
+//               preload.className.add("preload-finish");
+//               sessionStorage.viewed = 1;
+//           }else{
+//             const preload = document.querySelector(".preload");
+//             preload.classList.add("preload-finish");
+//           }
+// });
 
 const clickMeNav = document.querySelector('#clickMeNav');
 let rotate = document.querySelector('.nav_chevron');
