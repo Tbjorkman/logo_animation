7a8855827bacc21bc49840559f13b86a94e8a5bc
Tabitha Bjorkman <tabithabjorkman@gmail.com>
2021-05-07 11:45:21 +0200

    fixed

--- logo_animation/logo_animation.js
+++ logo_animation/logo_animation.js
@@ -1,3 +1,3 @@
-gsap.from(".house_roof", { delay:0.9, opacity:0, duration: 0.7, easeOut: "power3.out", y:-700  });
+gsap.from(".house_roof", { delay:0.9, opacity:0, duration: 0.7, ease: "power3.out", y:-700  });
 
  
