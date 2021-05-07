43058fdfb4afda31f14c879d62c7a822ad382976
Tabitha Bjorkman <tabithabjorkman@gmail.com>
2021-05-07 11:33:01 +0200

    new button function

--- js/main.js
+++ js/main.js
@@ -1,9 +1,9 @@
 
 
-// window.addEventListener('load', () => {
-//     const preload = document.querySelector('.preload');
-//     preload.classList.add("preload-finish");
-// });
+window.addEventListener('load', () => {
+    const preload = document.querySelector('.preload');
+    preload.classList.add("preload-finish");
+});
 // window.addEventListener("load", () => {
 //           if (!sessionStorage.viewed){
 //               const preload = document.querySelector(".preload");
@@ -20,10 +20,34 @@ let rotate = document.querySelector('.nav_chevron');
 let dropDown = document.querySelector('#dropdown');
 
 function dropDownNav(){
-    console.log('this is shit');
     dropDown.classList.toggle('showNav');
     rotate.classList.toggle('rotate');
 }
 
 clickMeNav.addEventListener('click', dropDownNav);
 
+
+
+//let lockBC = document.querySelector("#lockButtonsContainer");
+let lockButtons = document.querySelectorAll("#lockButtonsContainer .lockButtons");
+//let locks = document.querySelectorAll('.lockImg');
+
+
+for (let lock of lockButtons){
+    lock.addEventListener('click', (e) =>{
+        console.log('blue moon');
+        const et = e.target;
+        const lockEON = document.querySelector(".lockEngagedOrNot");
+        const buttonBG = document.querySelector(".buttonBGColor")
+        if (lockEON)
+        {
+            lockEON.classList.remove("lockEngagedOrNot");
+            buttonBG.classList.remove("buttonBGColor")
+        }
+        et.classList.add("lockEngagedOrNot");
+        et.classList.add("buttonBGColor");
+    })
+}
+
+
+    
